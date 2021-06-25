29a6f08830d3770fc48f5ce60c38e871d388be3c
Johan Díaz <jgiandh46@gmail.com>
2021-06-24 23:02:20 -0500

    :lipstick: [ADD] Modificados estilos CSS

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -30,6 +31,12 @@ module.exports = {
                 filename: './index.html',
             }
         ),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: './src/styles/styles.css',
+                to: '',
+            }]
+        })
     ],
     devServer: {
         port: 6969,
